fcdaa4d65c31862ac28b3276e9d9151b440e3562
nrzhang115 <159680727+nrzhang115@users.noreply.github.com>
2024-03-10 13:41:07 +1100

    Modify file_pairs

--- utils/util.py
+++ utils/util.py
@@ -38,7 +38,7 @@ def load_folds_data(np_data_path, n_folds):
 
     files_dict = dict()
 
-    files_pairs = [[files[7],files[8],files[9]]]
+    files_pairs = [[files[7],files[8],files[9]], [files[0],files[10],files[11]]]
     file_pair = []
     '''for i in range(n_folds * 32):
         file_pair.append(files[i])
